41a6e65ec1e96d7582d91f53593de0bf52df3097
Katie Liu <katieliu59@gmail.com>
2023-01-28 01:37:21 -0500

    Implement exercise solutions

--- kata-15.js
+++ kata-15.js
@@ -2,13 +2,14 @@ const organizeInstructors = function (instructors) {
 	//initialize empty object to store instructors organized by course
 	let organizeByCourse = {};
 
+    //loop through list of instructors
 	for (let i = 0; i < instructors.length; i++) {
 		//check if organizeByCourse contain a property of the instructor course
 		//if it doesn't, it creates a new property with that name and set it's value to array of instructor's name
 		if (!organizeByCourse.hasOwnProperty(instructors[i].course)) {
 			organizeByCourse[instructors[i].course] = [instructors[i].name];
 		} else {
-			//if course exists in object, we push the instructor name to the array of the property
+		//if course exists in object, we push the instructor name to the array of the property
 			organizeByCourse[instructors[i].course].push(instructors[i].name);
 		}
 	}
